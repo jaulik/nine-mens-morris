d0ffd4ff53ac6216c40cfa88fad0abcbbe552ba4
Jan Skořepa <skorepa-jan@seznam.cz>
2025-04-18 13:32:27 +0200

    add cursor

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -6,16 +6,16 @@ INSERT INTO players (player_id, name) VALUES (seq_players_id.NEXTVAL, 'Miluse');
 
 INSERT INTO games (game_id, player1_id, player2_id, start_time, end_time, winner, total_moves)
 VALUES (seq_game_id.NEXTVAL, 1, 2, TO_DATE('2025-04-02 15:00', 'YYYY-MM-DD HH24:MI'),
-        TO_DATE('2025-04-02 15:45', 'YYYY-MM-DD HH24:MI'), 1, 23);
+        TO_DATE('2025-04-02 15:29', 'YYYY-MM-DD HH24:MI'), 1, 23);
 INSERT INTO games (game_id, player1_id, player2_id, start_time, end_time, winner, total_moves)
 VALUES (seq_game_id.NEXTVAL, 3, 4, TO_DATE('2025-04-02 16:00', 'YYYY-MM-DD HH24:MI'),
-        TO_DATE('2025-04-02 16:45', 'YYYY-MM-DD HH24:MI'), 4, 28);
+        TO_DATE('2025-04-02 16:16', 'YYYY-MM-DD HH24:MI'), 4, 28);
 INSERT INTO games (game_id, player1_id, player2_id, start_time, end_time, winner, total_moves)
 VALUES (seq_game_id.NEXTVAL, 1, 3, TO_DATE('2025-04-02 17:00', 'YYYY-MM-DD HH24:MI'),
-        TO_DATE('2025-04-02 17:45', 'YYYY-MM-DD HH24:MI'), 3, 31);
+        TO_DATE('2025-04-02 17:35', 'YYYY-MM-DD HH24:MI'), 3, 31);
 INSERT INTO games (game_id, player1_id, player2_id, start_time, end_time, winner, total_moves)
 VALUES (seq_game_id.NEXTVAL, 4, 2, TO_DATE('2025-04-02 18:00', 'YYYY-MM-DD HH24:MI'),
-        TO_DATE('2025-04-02 18:45', 'YYYY-MM-DD HH24:MI'), 4, 18);
+        TO_DATE('2025-04-02 18:30', 'YYYY-MM-DD HH24:MI'), 4, 18);
 
 -- test data, few moves for each game
 INSERT INTO moves (move_id, game_id, player_id, position_from, position_to)
