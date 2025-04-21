f65e8361b2da7e2f1eb747316efc9b69bb4d4fb6
Jan Skořepa <skorepa-jan@seznam.cz>
2025-04-21 22:11:23 +0200

    Working on sqlite scripts and python functions (managing db and interacting with db).

---
+++ sql/drop_sqlite_db.sql
@@ -0,0 +1,6 @@
+PRAGMA foreign_keys = OFF;
+
+-- Drop existing tables (triggers are dropped automatically)
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS statistics;
+DROP TABLE IF EXISTS players;
