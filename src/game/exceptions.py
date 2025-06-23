a21f305ae3e26b2c5f7256d0603579d329ea297f
Jan Skořepa <skorepa-jan@seznam.cz>
2025-06-23 22:13:30 +0200

    added board logic, exception

--- src/game/exceptions.py
+++ src/game/exceptions.py
@@ -6,3 +6,12 @@ class PositionAlreadyOccupiedError(Exception):
         self.position_occupied_by = position.get_occupied_by()
         super().__init__(f"Position {self.position_id} is already occupied by player {self.position_occupied_by}.")
 
+class InvalidPieceRemovalError(Exception):
+    def __init__(self, position_id: int, attempted_by: int, actual_owner: int | None):
+        message = (f"Cannot remove piece at position {position_id}. "
+                   f"Attempted by player {attempted_by}, but position is "
+                   f"{'empty' if actual_owner is None else f'occupied by player {actual_owner}'}.")
+        super().__init__(message)
+        self.position_id = position_id
+        self.attempted_by = attempted_by
+        self.actual_owner = actual_owner
---
+++ src/game/game_state.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class GameState(Enum):
+    PLACING = 1
+    MOVING = 2
+    FLYING = 3
