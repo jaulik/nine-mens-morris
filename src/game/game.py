2a24972c1c56978a45fefd2a7d346e680706d89c
Jan Skořepa <skorepa-jan@seznam.cz>
2025-06-24 21:48:00 +0200

    game simplified

--- src/game/game.py
+++ src/game/game.py
@@ -53,15 +53,14 @@ class Game:
         if action == "place":
             try:
                 pos_id = args[0]
-                self.place_piece(pos_id)
+                self.__board.place_piece(self.get_current_player(), pos_id)
                 self.get_current_player().decrement_in_hand()
                 self.get_current_player().increment_on_board()
 
                 if self.__board.is_mill(pos_id, self.get_current_player().get_id()):
                     self.__mills_formed = True
                 else:
-                    self.__mills_formed = False
-                self.switch_current_player()
+                    self.switch_current_player()
             
             except (PositionOutOfBoundsError, PositionAlreadyOccupiedError) as e:
                 print(e)
@@ -69,19 +68,21 @@ class Game:
         elif action == "move":
             try:
                 from_pos_id, to_pos_id = args
-                self.move_piece(from_pos_id, to_pos_id)
+                self.__board.move_piece(from_pos_id, to_pos_id, self.get_current_player())
                 if self.__board.is_mill(to_pos_id, self.get_current_player().get_id()):
                     self.__mills_formed = True
                 else:
-                    self.__mills_formed = False
-                self.switch_current_player()
+                    self.switch_current_player()
             
             except (PositionOutOfBoundsError, PositionAlreadyOccupiedError, InvalidMoveError) as e:
                 print(e)
 
         elif action == "remove":
             try:
-                self.remove_piece(args[0])
+                self.__board.remove_piece(args[0],
+                                          self.get_current_player(),
+                                          self.get_opposite_player())
+
                 self.__mills_formed = False
                 self.switch_current_player()
                 self.get_opposite_player().decrement_on_board()
@@ -91,39 +92,3 @@ class Game:
         else:
             raise ValueError(f"Unknown action '{action}'")
 
-
-    def place_piece(self, position_id: int) -> None:
-        board = self.__board
-        if not (0 <= position_id <= 23):
-            raise PositionOutOfBoundsError(position_id)
-        board.place_piece(self.get_current_player(), position_id)
-        
-
-    def move_piece(self, from_pos_id: int, to_pos_id: int) -> None:
-        if not (0 <= from_pos_id <= 23 and 0 <= to_pos_id <= 23):
-            raise PositionOutOfBoundsError(from_pos_id
-                                           if from_pos_id < 0 or from_pos_id > 23 else to_pos_id)
-
-        board = self.__board
-        from_pos = board.get_position(from_pos_id)
-        to_pos = board.get_position(to_pos_id)
-
-        if from_pos.get_occupied_by() != self.__current_player.get_id():
-            raise InvalidMoveError(from_pos, to_pos)
-        
-        board.move_piece(from_pos_id, to_pos_id)
-
-    def remove_piece(self, position_id: int) -> None:
-        if not (0 <= position_id <= 23):
-            raise PositionOutOfBoundsError(position_id)
-        
-        target_pos = self.__board.get_position(position_id)
-        occupied_by = target_pos.get_occupied_by()
-
-        if occupied_by is None or occupied_by == self.get_current_player().get_id():
-            raise InvalidPieceRemovalError(position_id, self.get_current_player().get_id(),
-                                           None if occupied_by is None else occupied_by.get_id())
-
-        self.__board.remove_piece(position_id,
-                                  self.get_current_player().get_id(),
-                                  self.get_opposite_player().get_id())
