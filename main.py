6386c8077b300dbf09fc1a6a9de826c67f5e0b89
Jan Skořepa <skorepa-jan@seznam.cz>
2025-11-25 14:46:04 +0100

    add login logic

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from src.game.player import Player
 from src.game.game import Game
 
-from src.db.manage_db import add_player, start_game, end_game, get_statistics
+from src.db.manage_db import add_player, start_game, end_game, get_statistics, get_player_id_by_name
 
 if __name__ == "__main__":
-    # TODO: if player already exists enter name and id and proceed (SELECT)
 
     print("""
 Welcome to Nine Men's Morris!
@@ -38,18 +37,33 @@ Game Rules & Instructions:
     """)
 
     print("---------------------------------------------")
-    name1 = input("Enter name of player 1: ")
-    id1 = add_player(name1)
-    print("Your ID is: ", id1)
-    player1 = Player(name1, id1)
-    
-    name2 = input("Enter name of player 2: ")
-    id2 = add_player(name2)
-    print("Your ID is: ", id2)
-    player2 = Player(name2, id2)
 
-    game_id = start_game(id1, id2)
+    def login_player(player_number: int) -> Player:
+        while True:
+            name = input(f"Enter name of player {player_number}: ").strip()
+            if not name:
+                print("Name cannot be empty.")
+                continue
+
+            existing_id = get_player_id_by_name(name)
+            if existing_id is None:
+                new_player_id = add_player(name)
+                print(f" -> New player created. Your ID is: {new_player_id}")
+                return Player(name, new_player_id)
+
+            print(f" -> Welcome back, {name}! Your ID is {existing_id}")
+            return Player(name, existing_id)
+
+    player1 = login_player(1)
+    while True:
+        player2 = login_player(2)
+        if player1.get_id() == player2.get_id():
+            print("Player 2 cannot be the same as Player 1. Please login as a different user.")
+        else:
+            break
+
+    game_id = start_game(player1.get_id(), player2.get_id())
     game = Game(player1, player2)
     
     winner = game.play()
-    end_game(winner.get_id(), game.get_rounds(), game_id)
+    end_game(winner.get_id() if winner else None, game.get_rounds(), game_id)
