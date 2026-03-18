8f60599bdb234399f854a59f74a26bd837d36c63
Jan Skořepa <skorepa-jan@seznam.cz>
2026-03-18 12:44:16 +0100

    fix legal actions method

--- src/nine_mens_morris/game/game.py
+++ src/nine_mens_morris/game/game.py
@@ -91,11 +91,18 @@ class Game:
                 raise ValueError(f"Unknown action '{action}'")
 
     def legal_actions_for_current_player(self) -> list[Action]:
+        if self.__mills_formed:
+            opponent_positions = self.__board.positions_occupied_by(self.get_opposite_player())
+            not_in_mill = [pos for pos in opponent_positions if not self.__board.is_in_mill(pos, self.get_opposite_player())]
+
+            if len(not_in_mill) > 0:
+                return [Remove(pos) for pos in not_in_mill]
+            # only allow removing stones that are part of opponents mill
+            # if opponent has no stones that are not a part of a mill
+            return [Remove(pos) for pos in opponent_positions]
+
         if self.get_state() == GameState.PLACING:
             return [Place(pos) for pos in self.__board.empty_positions()]
-
-        if self.__mills_formed:
-            return [Remove(pos) for pos in self.__board.positions_occupied_by(self.get_opposite_player())]
 
         if self.get_state() == GameState.MOVING or self.get_state() == GameState.JUMPING:
             actions: list[Action] = []
